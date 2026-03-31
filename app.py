cae636f3cdf118bf01ce47aacfce0d1f771e39a3
jmgonzalez <jmgonzalezro@gmail.com>
2026-03-31 18:37:51 +0200

    New night and day icon and cv as terminal

--- app.py
+++ app.py
@@ -1,30 +1,31 @@
 #!/venv/bin/python3
 
 
-from markupsafe import Markup
-from flask import Flask, render_template, request, make_response
-from datetime import datetime
-import markdown
-from markdown.extensions.footnotes import FootnoteExtension
-import re
 import os
+import re
+from datetime import datetime
+
 import frontmatter
+import markdown
+from flask import Flask, make_response, render_template, request
+from markdown.extensions.footnotes import FootnoteExtension
+from markupsafe import Markup
 
 app = Flask(__name__)
 
 
-POSTS_DIR = 'posts'
+POSTS_DIR = "posts"
 
 
-@app.route('/')
+@app.route("/")
 def index():
     posts = []
     for post in os.listdir(POSTS_DIR):
         if post.endswith(".md"):
             post_path = os.path.join(POSTS_DIR, post)
-            with open(post_path, 'r') as f:
+            with open(post_path, "r") as f:
                 parsed_post = frontmatter.load(f)
-                post_date = parsed_post.get('date', datetime.now()),
+                post_date = (parsed_post.get("date", datetime.now()),)
                 if post_date:
                     pass
                 elif post_date:
@@ -33,7 +34,7 @@ def index():
 
     sorted_posts = sorted(posts, key=lambda x: x[1], reverse=True)
 
-    page = request.args.get('page', 1, type=int)
+    page = request.args.get("page", 1, type=int)
     per_page = 5
     start = (page - 1) * per_page
     end = start + per_page
@@ -42,59 +43,81 @@ def index():
     processed_posts = [
         {
             "url": post[0][:-3],
-            "title": post[0][:-3].replace('_', ' '),
-            'date': post[1],
+            "title": post[0][:-3].replace("_", " "),
+            "date": post[1],
         }
         for post in paginated_posts
     ]
     total_pages = (len(sorted_posts) + per_page - 1) // per_page
 
-    response = make_response(render_template('index.html', posts=processed_posts, page=page, total_pages=total_pages))
+    response = make_response(
+        render_template(
+            "index.html", posts=processed_posts, page=page, total_pages=total_pages
+        )
+    )
     return set_no_cache_headers(response)
 
 
 def set_no_cache_headers(response):
-    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-    response.headers['Pragma'] = 'no-cache'
-    response.headers['Expires'] = '0'
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
     return response
 
 
-@app.route('/posts/<post_name>')
+@app.route("/posts/<post_name>")
 def post(post_name):
-    if '..' in post_name or not os.path.abspath(f"{POSTS_DIR}/{post_name}.md").startswith(os.path.abspath(POSTS_DIR)):
-        return 'Post no permitido.', 403
+    if ".." in post_name or not os.path.abspath(
+        f"{POSTS_DIR}/{post_name}.md"
+    ).startswith(os.path.abspath(POSTS_DIR)):
+        return "Post no permitido.", 403
 
-    post_path = f'{POSTS_DIR}/{post_name}.md'
+    post_path = f"{POSTS_DIR}/{post_name}.md"
     if not os.path.exists(post_path):
-        return 'Post not found', 404
+        return "Post not found", 404
 
-    with open(post_path, 'r') as f:
+    with open(post_path, "r") as f:
         parsed_post = frontmatter.load(f)
         content = parsed_post.content
-        title = parsed_post.get('title', post_name.replace('_', ' '))
-        subtitle = parsed_post.get('subtitle')
-        date = parsed_post.get('date')
-        if date:
-            pass
-            # date = datetime.strptime(date, '%Y-%m-%d').strftime('%Y-%m-%d')
-        else:
-            date = datetime.fromtimestamp(os.path.getctime(post_path)).strftime('%Y-%m-%d')
+        title = parsed_post.get("title", post_name.replace("_", " "))
+        subtitle = parsed_post.get("subtitle")
+        date = parsed_post.get("date")
+        if not date:
+            date = datetime.fromtimestamp(os.path.getctime(post_path)).strftime(
+                "%Y-%m-%d"
+            )
 
         if subtitle is None:
-            subtitle = ' '
+            subtitle = " "
 
     # Reemplazar las etiquetas {marginnote} y {/marginnote} con HTML
-    content = re.sub(r'\{marginnote\}(.*?)\{\/marginnote\}', r'<span class="marginnote">\1</span>', content)
+    content = re.sub(
+        r"\{marginnote\}(.*?)\{\/marginnote\}",
+        r'<span class="marginnote">\1</span>',
+        content,
+    )
 
-    html_content = Markup(markdown.markdown(content, extensions=[FootnoteExtension(), 'extra']))
-    response = make_response(render_template('post.html', content=html_content, title=title, date=date, subtitle=subtitle))
+    html_content = Markup(
+        markdown.markdown(content, extensions=[FootnoteExtension(), "extra"])
+    )
+    response = make_response(
+        render_template(
+            "post.html", content=html_content, title=title, date=date, subtitle=subtitle
+        )
+    )
     return set_no_cache_headers(response)
 
 
-@app.route('/debug/posts')
-def debug_posts():
-  return 'Esta ruta ya no está disponible.', 404
+@app.route("/cv-terminal")
+def cv_terminal():
+    response = make_response(render_template("terminal.html"))
+    return set_no_cache_headers(response)
 
-if __name__ == '__main__':
+
+@app.route("/debug/posts")
+def debug_posts():
+    return "Esta ruta ya no está disponible.", 404
+
+
+if __name__ == "__main__":
     app.run(debug=False)
